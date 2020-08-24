bb00870fb08c36443db6e5fd1c660b7e272bd3c9
Vmcg22 <54409821+Vmcg22@users.noreply.github.com>
2020-08-23 21:45:45 -0500

    Se dejo funcionando la creación de usuarios del modelo (User)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    datos_user = params[:user]
+
+    nombre = datos_user[:nombre]
+    telefono = datos_user[:telefono]
+    direccion = datos_user[:direccion]
+    observaciones = datos_user[:observaciones]
+
+    @user = User.create(nombre: nombre, telefono: telefono, direccion: direccion, observaciones: observaciones)
+
+    redirect_to "/users/#{@user.id}"
+    
   end
 
   def edit
